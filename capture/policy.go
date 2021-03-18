980e5dd12880876624e36a9fe6e6c74c9162a4a1
ZYA <zyazhb@yahoo.com>
2021-03-18 16:22:39 +0800

    reframe

--- capture/policy.go
+++ capture/policy.go
@@ -1,15 +1,24 @@
 package capture
 
 import (
+	"fmt"
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 )
 
-func RunPolicy(w gopacket.Packet) {
-	SameSrcIp(w)
+func RunPolicy(packet gopacket.Packet) {
+	SameSrcIp(packet)
 }
 
-func SameSrcIp(w gopacket.Packet) {
-	print(w)
+func SameSrcIp(packet gopacket.Packet) {
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		fmt.Println("IPv4 layer detected.")
+		ip, _ := ipLayer.(*layers.IPv4)
+		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("Protocol: ", ip.Protocol)
+		fmt.Println()
+	}
 }
 
 func SynFlood() {}
