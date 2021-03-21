665f49a49164e632dbddc363f3bc4579b9a361c0
Dizzy-K <1533022595@qq.com>
2021-03-21 11:16:24 +0800

    style💄 dDDos基本前端框架完成

---
+++ ui/assets/js/scripts.js
@@ -0,0 +1,19 @@
+"use strict";
+var options = true,
+    scrollToElement = {
+        scrollSpeed: 700
+    },
+    GoogleMapAPI = {
+        key: "AIzaSyBKAtINYLdAjSg-qBuhCndrkN7BBu22dGc",
+        language: "",
+        region: "",
+        disableDefaultUI: false,
+        markerIcon: "",
+        customMapStyle: ""
+    },
+    slider = {
+        interval: 5000,
+    },
+    scrollbar = {
+        zindex: 12
+    };
