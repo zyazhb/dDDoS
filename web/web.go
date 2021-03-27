cf13336dc9ced713e9982b222b4308f70dd158c7
Dizzy-K <1533022595@qq.com>
2021-03-27 20:02:22 +0800

    style💄 请无视掉这条，谢谢

--- web/web.go
+++ web/web.go
@@ -1,14 +1,25 @@
-package web
+package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"/* 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie" */
+	"github.com/gin-gonic/gin"
+)
 
-func RunWeb() {
-	r := gin.Default()
-	
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("../ui/*.html")
+	router.StaticFS("/js", http.Dir("../ui/assets/js"))
+	router.StaticFS("/css", http.Dir("../ui/assets/css"))
+	router.StaticFS("/img", http.Dir("../ui/assets/img"))
+	router.StaticFS("/plugins", http.Dir("../ui/assets/plugins"))
+	// 如果使用 LoadHTMLFiles 的话这么做（需要列举所有需要加载的文件，不如上述 LoadHTMLGlob 模式匹配方便）：
+	// router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/index", func(c *gin.Context) {
+	  c.HTML(http.StatusOK, "index.html", gin.H{
+		"title": "Main website",
+	  })
 	})
-	r.Run(":80")
-}
+	router.Run(":8080")
+  }
