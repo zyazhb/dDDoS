11d45b528b851ec619c5b821b8718e5acab80b91
mashiro01 <m1904727480@outlook.com>
2021-03-31 14:11:29 +0800

    合约检测合法性基本完成

--- node/contract/contracts/Ddos.sol
+++ node/contract/contracts/Ddos.sol
@@ -9,7 +9,12 @@ pragma experimental ABIEncoderV2;
 // import "https://gist.githubusercontent.com/ageyev/779797061490f5be64fb02e978feb6ac/raw/dbd08a23531b784186bc4bfffcadefcb23db88e9/StringsAndBytes.sol";
 
 contract Auther {
+    /*
+        address: Fringe node -> Account address
+        bool: isRegist
+    */
     mapping(address => bool) nodes;
+    address[] public nodesAddr;
 
     address private chairman;
 
@@ -32,6 +37,7 @@ contract Auther {
         require(!nodes[addr], "The node has been registed!");
 
         nodes[addr] = true;
+        nodesAddr.push(addr);
     }
 }
 
@@ -43,10 +49,12 @@ contract DDoS is Auther {
         string targetIP;
         uint speed;
         string timestamp;
-        bool isDDoS;
+        string descrptions;
 
         bool isUsed;
     }
+
+    // Rconn -> Actions
 
     mapping(uint => Rconn) rconns;
 
@@ -54,19 +62,11 @@ contract DDoS is Auther {
         require(!rconns[eventID].isUsed , "This eventID has been used , can't insert into rconns");
 
         rconns[eventID].commiterAddr = addr;
-        rconns[eventID].targetIP = rconn[0];
-        rconns[eventID].speed = sp;
-<<<<<<< HEAD
-<<<<<<< HEAD:node/contract/contracts/Ddos.sol
-        rconns[eventID].timestamp = rconn[1];
-=======
-        rconns[eventID].timestamp = rconn[2];
->>>>>>> 515b151 (初步完成链与流量监测的连接):node/contract/contracts/Migrations.sol
-=======
-        rconns[eventID].timestamp = rconn[1];
->>>>>>> 515b151 (初步完成链与流量监测的连接)
+        rconns[eventID].speed        = sp;
+        rconns[eventID].targetIP     = rconn[0];
+        rconns[eventID].timestamp    = rconn[1];
+        rconns[eventID].descrptions  = rconn[2];
 
-        rconns[eventID].isDDoS = false;
         rconns[eventID].isUsed = true;
 
         emit msgConn(eventID, addr, "insert", "Rconn");
@@ -76,10 +76,12 @@ contract DDoS is Auther {
         return rconns[eventID];
     }
 
+    // Rddos -> Actions
+
     mapping(uint => Rconn) rddoses;
     uint[] public events;
 
-    function insertRddos(uint eventID) onlyRegisted public {
+    function insertRddos(uint eventID) onlyRegisted onlyValidAttack public {
         require(!rconns[eventID].isUsed, "This eventID has been used , can't insert into rddoses!");
 
         events.push(eventID);
@@ -87,5 +89,33 @@ contract DDoS is Auther {
         delete rconns[eventID];
 
         emit msgConn(eventID, rddoses[eventID].commiterAddr, "insert", "Rddos");
+    }
+
+    // Valid Checker
+
+    mapping(address => bool) valids;
+
+    constructor() public {
+        for (uint i = 0; i < nodesAddr.length; i++) {
+            valids[nodesAddr[i]] = false;
+        }
+    }
+
+    function reCheckDDos(Rconn memory rconn, uint threshold) onlyRegisted public {
+        require(nodes[rconn.commiterAddr], "The commiter's address is not registed!");
+        require(nodes[msg.sender], "The node's address is not registed!");
+
+        if(rconn.speed >= threshold) {
+            valids[msg.sender] = true;
+        }
+    }
+
+    modifier onlyValidAttack() {
+        for (uint i = 0; i < nodesAddr.length; i++) {
+            if (valids[nodesAddr[i]] == false) {
+                revert();
+            }
+        }
+        _;
     }
 }
