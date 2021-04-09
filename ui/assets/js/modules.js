b70380dce29ebe4a879fb018da6cd5a1d88a9913
Dizzy-K <1533022595@qq.com>
2021-04-09 14:21:01 +0800

    style💄 class:span10位置修不好了呜呜呜

--- ui/assets/js/modules.js
+++ ui/assets/js/modules.js
@@ -1890,11 +1890,50 @@ function chartJSInit() {
         }
 
         if ($("#achart-line").length) {
-            let achart = new ApexCharts(
-                document.querySelector("#achart-line"),
-                options_act
-            );
-            achart.render();
+            $(document).ready(function(){
+                var canvas = document.getElementById('canvas');
+                var stage = new JTopo.Stage(canvas);
+                //显示工具栏
+                showJTopoToobar(stage);
+                var scene = new JTopo.Scene();
+                stage.add(scene);
+                scene.background = '/bg.jpg';
+                
+                var cloudNode = new JTopo.Node('root');
+                cloudNode.setSize(30, 26);
+                cloudNode.setLocation(360,230);            
+                cloudNode.layout = {type: 'circle', radius: 150};
+                
+                scene.add(cloudNode);
+                
+                for(var i=1; i<4; i++){
+                    var node = new JTopo.CircleNode('host' + i);
+                    node.fillStyle = '200,255,0';
+                    node.radius = 15;
+                    node.setLocation(scene.width * Math.random(), scene.height * Math.random());
+                    node.layout = {type: 'circle', radius: 80};
+                    
+                    scene.add(node);                                
+                    var link = new JTopo.Link(cloudNode, node);
+                    scene.add(link);
+                    
+                    for(var j=0; j<6; j++){
+                        var vmNode = new JTopo.CircleNode('vm-' + i + '-' + j);
+                        vmNode.radius = 10;
+                        vmNode.fillStyle = '255,255,0';
+                        vmNode.setLocation(scene.width * Math.random(), scene.height * Math.random());
+                        scene.add(vmNode);                                
+                        scene.add(new JTopo.Link(node, vmNode));                            
+                    }
+                }
+                JTopo.layout.layoutNode(scene, cloudNode, true);
+                
+                scene.addEventListener('mouseup', function(e){
+                    if(e.target && e.target.layout){
+                        JTopo.layout.layoutNode(scene, e.target, true);    
+                    }                
+                });
+            }); 
         }
 
         /* chart-bar */
