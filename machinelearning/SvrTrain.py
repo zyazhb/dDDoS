d57e8e961ae3576b12807eb35ce420f13cb3a253
windy810 <2609957581@qq.com>
2021-07-26 13:43:40 +0800

    fix train value

--- machinelearning/SvrTrain.py
+++ machinelearning/SvrTrain.py
@@ -31,14 +31,14 @@ for i in range(99):
         origin_data.iloc[i:,79]=1
 
 X = origin_data.iloc[:,:].values
-Y = origin_data.iloc[:,78].values
+Y = origin_data.iloc[:,79].values
 # print(type(Y))
 # print(type(Y.values))
 
 # 总特征  按照特征的重要性排序的所有特征
 all_feature = [57, 55, 67, 75, 39, 56, 43, 29, 44, 19, 35, 20, 48, 16, 49, 34, 47, 63, 71, 66, 79, 68, 74, 72, 65, 77, 69, 31, 21, 25, 30, 27, 22, 1, 3, 4, 59, 60, 64, 62, 17, 45, 52, 41, 38, 9, 23, 10, 51, 24, 11, 12, 13, 18, 50, 40, 53, 14, 8, 6, 5, 7, 15, 32, 42, 58, 37, 54, 46, 36, 61, 33, 2, 28, 26, 73, 70, 78, 76, 0]
 # 这里我们选取前三个特征
-topN_feature = all_feature[:40]
+topN_feature = all_feature[:30]
 print(topN_feature)
  
 # 获取重要特征的数据
