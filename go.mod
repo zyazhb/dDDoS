66680e6acb19ecb985462664d716d29f08f1d69e
mashiro01 <m1904727480@outlook.com>
2021-07-27 08:56:20 +0800

    简单修改合约内容，添加代币合约

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/ethereum/go-ethereum v1.10.2
 	github.com/gin-gonic/gin v1.7.1
 	github.com/google/gopacket v1.1.19
+	gopkg.in/yaml.v2 v2.3.0
 )
---
+++ node/util.go
@@ -0,0 +1,46 @@
+package node
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type ClientConfig struct {
+	chainAddress 	string 	`yaml:"chain-address"`
+	chainPort		int		`yaml:"chain-port"`
+
+	Server struct {
+		contractAddr	string	`yaml:"contract-address"`
+	}
+
+	Client struct {
+		clientAddr		string	`yaml:"address"`
+	}
+}
+
+const (
+	configFilePath = "../config/config.yaml"
+)
+
+var (
+	Conf *ClientConfig
+)
+
+func init() {
+	yamlFile, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		log.Fatalln("Read config file error!")
+		return
+	}
+
+	conf := new(ClientConfig)
+	err = yaml.Unmarshal(yamlFile, conf)
+	if err != nil {
+		log.Fatalln("Faile to unmarshal config file!")
+		return
+	}
+
+	Conf = conf
+}
